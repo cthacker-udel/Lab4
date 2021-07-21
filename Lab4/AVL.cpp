0e38bab9b583c746ce9d5b4fb24f609e2a661e17
cthacker-udel <cthacker@udel.edu>
2021-07-20 21:49:49 -0400

    first commit

---
+++ Lab4/AVL.cpp
@@ -0,0 +1,431 @@
+
+/**************************************************************/
+/* Part 1:                                                    */ /* Name the methods below with the correct name from the list */
+/* below:                                                     */
+/*    find                                                    */
+/*    insert                                                  */
+/*    printTreePre                                            */
+/*    printTreeIO                                             */
+/*    printTreePost                                           */
+/*                                                            */
+/* Once you have named them, move them to into the AVL Class  */
+/* Definition file (the .cpp), below the constructors (scroll */
+/* down and you'll see the constructors)                      */
+/**************************************************************/
+/********************* method 1 *******************************/
+
+/*******************End of Naming Methods*********************/
+/* Now move the methods above (with the blanks filled in     */
+/* correctly below the constructors, below                   */
+/*************************************************************/
+
+/*************************************************************/
+/*Part 2                                                     */
+/* Now you must do two things:                               */
+/* 1) add a count to the find method such that it counts how */
+/*    many comparisions you do every time you find an        */
+/*    abbreviation; and                                      */
+/* 2) in the insert method, add a call to the setHeight      */
+/*    method in the appropriate place(s) so that after you   */
+/*    inserted, the setHeight method adjusts the heights of  */
+/*    the inserted node's ancestors.
+/*************************************************************/
+
+/*************************************************************/
+/*Part 3                                                     */
+/* Write the methods (in red, below) that are part of the    */
+/* AVL Tree class                                            */
+/*************************************************************/
+
+#include "AVL.hpp"
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+/*************************************************************/
+/* Constructors 
+   debug is a boolean flag that, if set, will result in the 
+   printing of a number of debug statements.  If it isn't
+   set, the code will print without the debug statements.
+*/
+AVL::AVL() {
+	root = NULL;
+	debug = false;
+}
+
+AVL::AVL(bool debflag) {
+	root = NULL;
+	debug = debflag;
+}
+
+AVL::AVL(string ab, string d) {
+	root = new TNode(ab, d, false);
+	debug = false;
+
+}
+
+AVL::AVL(string ab, string d, bool debflag) {
+	root = new TNode(ab, d, debflag);
+	debug = debflag;
+
+}
+/*************************************************************/
+/*Methods you have to write                                  */
+/*These methods are specifically for an AVL tree, which means*/
+/*they involve keeping track of a node's height and balance, */
+/*and rotating when a node becomes unbalanced                */
+/*WATCH OUT FOR:                                             */
+/*   NULL nodes (they don't have fields                      */
+/*   Attaching parents properly, especially during rotations */
+/*************************************************************/
+int AVL::getBalance(TNode *tmp) {
+/* method that finds the balance of a node tmp and returns that balance as an int
+*/
+
+	if(tmp->left == NULL && tmp->right == NULL){
+		return 0;
+	}
+	else if(tmp->right == NULL){
+		return tmp->left->height - 0;
+	}
+	else if(tmp->left == NULL){
+		return 0 - tmp->right->height;
+	}
+	else{
+		return tmp->left->height - tmp->right->height;
+	}
+
+}
+
+TNode *AVL::rotateRight(TNode *tmp) {
+/* rotates right around node tmp and returns the node rotated up.  Note: this method must reset the heights of the node rotated down and the nodes rotated up, and you must reset the heights of all nodes that are ancestors of the node rotated down.  You will also need to reattach the newly rotated up node to the rest of the tree either in this method or in setheights.
+*/
+	cout << "\t rotate right around " << tmp->abbr << endl;
+	TNode *movingUpNode = tmp->left;
+	tmp->left = movingUpNode->right;
+
+	if(tmp->left != NULL){
+		tmp->left->parent = tmp;
+	}
+
+	movingUpNode->right = tmp;
+
+	movingUpNode->parent = tmp->parent;
+	tmp->parent = movingUpNode;
+
+	if(root == tmp){
+		root = movingUpNode;
+	}
+
+	if(movingUpNode->parent != NULL){
+
+		if(movingUpNode->abbr > movingUpNode->parent->abbr){
+			movingUpNode->parent->right = movingUpNode;
+		}
+		else{
+			movingUpNode->parent->left = movingUpNode;
+		}
+	}
+
+	//if(tmp->left != NULL){
+	//	tmp->left->parent = tmp;
+	//}
+
+	setHeight(tmp->left);
+	setHeight(tmp->right);
+	setHeight(tmp);
+	setHeight(movingUpNode);
+
+	return movingUpNode;
+
+}
+
+TNode *AVL::rotateLeft(TNode *tmp) {
+/* rotates down around node tmp and returns the node rotated up.  Note: this method must reset the heights of the node rotated down and the nodes rotated up, and you must reset the heights of all nodes that are ancestors of the node rotated down. You will also need to reattach the newly rotated up node to the rest of the tree either in this method or in setheights.
+*/
+	cout << "\t rotate left around " << tmp->abbr << endl;
+
+	TNode *movingUpNode = tmp->right;
+	tmp->right = tmp->right->left;
+	movingUpNode->left = tmp;
+
+	movingUpNode->parent = tmp->parent;
+	tmp->parent = movingUpNode;
+
+
+	if(root == tmp){
+		root = movingUpNode;
+	}
+
+	if(movingUpNode->parent != NULL){
+		if(movingUpNode->abbr > movingUpNode->parent->abbr){
+			movingUpNode->parent->right = movingUpNode;
+		}
+		else{
+			movingUpNode->parent->left = movingUpNode;
+		}
+	}
+
+
+	//if(movingUpNode->left != NULL){
+	//	movingUpNode->left->parent = tmp;
+	//}
+
+	setHeight(tmp->left);
+	setHeight(tmp->right);
+	setHeight(tmp);
+	setHeight(movingUpNode);
+	return movingUpNode;
+}
+
+void AVL::setHeight(TNode *tmp) {
+
+	// check if node is leaf
+	while(tmp != NULL){
+		if(tmp == NULL){
+			return;
+		}
+		else if(tmp->left == NULL && tmp->right == NULL){
+			tmp->height = 1;
+		}
+		else{
+			if(tmp->left == NULL){
+				tmp->height = tmp->right->height+1;
+			}
+			else if(tmp->right == NULL){
+				tmp->height = tmp->left->height+1;
+			}
+			else{
+				tmp->height = tmp->left->height > tmp->right->height? tmp->left->height+1: tmp->left->height < tmp->right->height? tmp->right->height+1: tmp->left->height+1;
+			}
+		}
+		tmp = tmp->parent;
+	}
+
+/*
+This method sets the height of tmp and then of all the ancestors of tmp.  It stops when the height of a node does not change. Note that this method most likely calls getBalance and possibly the rotate methods, and may even set the newly rotated up node's parent attachement, although you could do that in the rotate method.
+*/ 
+}
+void AVL::printTreeIO() {
+	if (root == NULL ) {
+		cout << "Empty Tree" << endl;
+	}
+	else {
+		cout << endl<<"Printing In Order:" <<endl;
+		printTreeIO(root);
+	}
+}
+
+void AVL::printTreePre() {
+	if (root == NULL ) {
+		cout << "Empty Tree" << endl;
+	}
+	else {
+		cout << endl<<"Printing PreOrder:" <<endl;
+		printTreePre(root);
+	}
+}
+
+void AVL::printTreePost() {
+	if (root == NULL ) {
+		cout << "Empty Tree" << endl;
+	}
+	else {
+		cout << endl<<"Printing PostOrder:" <<endl;
+		printTreePost(root);
+	}
+}
+
+void AVL::clearTree() {
+	if (root == NULL) {
+		cout << "Tree already empty" << endl;
+	}
+	else {
+		cout << endl << "Clearing Tree:" << endl;
+		clearTree(root);
+		root = NULL;
+	}
+}
+void AVL::clearTree(TNode *tmp) {
+	if (tmp == NULL) {
+		return;
+	}
+	else {
+		clearTree(tmp->left);
+		clearTree(tmp->right);
+		tmp->printNode();
+		delete(tmp);
+	}
+}
+
+void AVL::printTreePre(TNode *tmp) {
+	if (tmp == NULL) {
+		return;
+	}
+	else {
+		tmp->printNode(debug);
+		printTreePre((tmp->left));
+		printTreePre((tmp->right));
+	}
+}
+/********************* method 2 *******************************/
+void AVL::printTreePost(TNode *tmp) {
+	if (tmp == NULL) {
+		return;
+	}
+	else {
+		printTreePost((tmp->left));
+		printTreePost((tmp->right));
+		tmp->printNode(debug);
+	}
+}
+/********************* method 3 *******************************/
+TNode *AVL::find(string a) {
+	int ct = 0;
+	if (root == NULL) {
+		if (debug) {
+		   cout << "root is null " << a << endl;
+		}
+		return NULL;
+	}
+	else {
+		TNode *tmp = root;
+		while (tmp != NULL) {
+			if (debug) {
+				cout <<tmp->abbr << " and "<<a << endl;
+			}
+			if (a < tmp->abbr) {
+				if (debug) {
+					cout << "going left " << endl;
+				}
+				if (tmp->left == NULL) {
+					return NULL;
+				}
+				else {
+					ct++;
+					tmp=tmp->left;
+				}
+			}
+			else if (a > tmp->abbr) {
+				if (debug) {
+					cout << "going right " << endl;
+				}
+				if (tmp->right == NULL) {
+					return NULL;
+				}
+				else {
+					ct++;
+					tmp=tmp->right;
+				}
+			}
+			else {
+				if (debug) {
+					cout << " in " << ct << endl;
+				}
+				return tmp;
+			}
+		}
+	}
+	return NULL;
+}
+/********************* method 4 *******************************/
+void AVL::printTreeIO(TNode *tmp) {
+	if (tmp == NULL) {
+		return;
+	}
+	else {
+		printTreeIO((tmp->left));
+		tmp->printNode(debug);
+		printTreeIO((tmp->right));
+	}
+}
+/********************* method 5 *******************************/
+bool AVL::insert(string ab, string d) {
+	TNode *newnode = new TNode(ab, d, debug);
+
+	if (root == NULL) {
+		if (debug ) {
+		    cout << "made new root" << endl;
+		}
+		root = newnode;
+		return true;
+	}
+	else {
+		TNode *tmp = root;
+		while (tmp != NULL){
+			if (newnode->abbr < tmp->abbr) {
+				if (tmp->left == NULL) {
+					tmp->left = newnode;
+					tmp->left->parent = tmp;
+					TNode *tmpParent = tmp;
+					while(tmpParent != NULL){
+						setHeight(tmpParent);
+
+						if(getBalance(tmpParent) > 1){
+							if(tmpParent->left != NULL && getBalance(tmpParent->left) < 0){
+								tmpParent = rotateLeft(tmpParent->left);
+								tmpParent = rotateRight(tmpParent->parent);
+							}
+							else{
+								tmpParent = rotateRight(tmpParent);
+							}
+						}
+						else if(getBalance(tmpParent) < -1){
+							if(tmpParent->right != NULL && getBalance(tmpParent->right) > 0){ // left-right
+								tmpParent = rotateRight(tmpParent->right);
+								tmpParent = rotateLeft(tmpParent->parent);
+							}
+							else{
+								tmpParent = rotateLeft(tmpParent);
+							}
+						}
+
+						tmpParent = tmpParent->parent;
+					}
+					return true;
+				}
+				else {
+					tmp=tmp->left;
+				}
+			}
+			else if (newnode->abbr > tmp->abbr) {
+				if (tmp->right == NULL) {
+					tmp->right = newnode;
+					tmp->right->parent = tmp;
+					TNode *tmpParent = tmp;
+					while(tmpParent != NULL){
+						setHeight(tmpParent);
+						if(getBalance(tmpParent) > 1){
+							if(tmpParent->left != NULL && getBalance(tmpParent->left) < 0){
+								tmpParent = rotateLeft(tmpParent->left);
+								tmpParent = rotateRight(tmpParent->parent);
+							}
+							else{
+								tmpParent = rotateRight(tmpParent);
+							}
+						}
+						else if(getBalance(tmpParent) < -1){
+							if(tmpParent->right != NULL && getBalance(tmpParent->right) > 0){ // left-right
+								tmpParent = rotateRight(tmpParent->right);
+								tmpParent = rotateLeft(tmpParent->parent);
+							}
+							else{
+								tmpParent = rotateLeft(tmpParent);
+							}
+						}
+						tmpParent = tmpParent->parent;
+					}
+					return true;
+				}
+				else {
+					tmp=tmp->right;
+				}
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	return false;
+}
